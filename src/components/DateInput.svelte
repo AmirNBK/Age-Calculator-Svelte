76ba360dcf34af59a3de5c68affc4f17a8de5b52
AmirHossein Norbakhsh <amircr7h@icloud.com>
2023-07-28 17:07:57 +0330

    installed tailwind and started developing date inputs

---
+++ src/components/DateInput.svelte
@@ -0,0 +1,9 @@
+<script>
+    export let title;
+    export let placeholder;
+</script>
+
+<div>
+    <h4 style="color : #8B8B8B" class="font-bold text-xs mb-2">{title}</h4>
+    <input {placeholder} style="border: 1px solid gray;" />
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,15 @@
+<script>
+    import "../app.css";
+    import DateInput from "../components/DateInput.svelte";
+</script>
 
-
-<div class="MainContainer bg-black">
-
-
+<div
+    class="MainContainer bg-white mx-auto rounded-md my-auto absolute left-2/4 top-2/4 w-8/12 h-4/6"
+    style="transform: translate(-50%,-50%); font-family : poppins"
+>
+    <div class="dates flex gap-20 m-7">
+        <DateInput title="DAY" placeholder="DD" />
+        <DateInput title="MONTH" placeholder="MM" />
+        <DateInput title="YEAR" placeholder="YYYY" />
+    </div>
 </div>
