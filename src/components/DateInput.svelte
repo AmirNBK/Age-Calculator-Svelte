d4f8b85dda1dbd4d9c44f4613cff26b7f32a2db6
AmirHossein Norbakhsh <amircr7h@icloud.com>
2023-07-28 23:32:01 +0330

    developed more of the date inputs

--- src/components/DateInput.svelte
+++ src/components/DateInput.svelte
@@ -5,5 +5,5 @@
 
 <div>
     <h4 style="color : #8B8B8B" class="font-bold text-xs mb-2">{title}</h4>
-    <input {placeholder} style="border: 1px solid gray;" />
+    <input {placeholder} class="text-3xl w-36 h-16 rounded-md px-4 font-semibold" style="border: 1px solid darkgray" />
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,18 @@
 <script>
     import "../app.css";
     import DateInput from "../components/DateInput.svelte";
+    import Arrow from '../../age-calculator-app-main/assets/images/arrow.svg'
 </script>
 
 <div
-    class="MainContainer bg-white mx-auto rounded-md my-auto absolute left-2/4 top-2/4 w-8/12 h-4/6"
-    style="transform: translate(-50%,-50%); font-family : poppins"
+    class="MainContainer bg-white mx-auto my-auto absolute left-2/4 top-2/4 w-6/12 h-4/6"
+    style="transform: translate(-50%,-50%); font-family : poppins; 	border-bottom-right-radius:250px"
 >
-    <div class="dates flex gap-20 m-7">
+    <div class="dates flex gap-8 m-7">
         <DateInput title="DAY" placeholder="DD" />
         <DateInput title="MONTH" placeholder="MM" />
         <DateInput title="YEAR" placeholder="YYYY" />
     </div>
+
+    <img src={Arrow} alt="arrow"/>
 </div>
