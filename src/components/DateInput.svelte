e43c1641435f2da985dd530e9d23368db9f13d8f
AmirHossein Norbakhsh <amircr7h@icloud.com>
2023-08-10 11:52:32 +0330

    developed the data passing from date inputs to the main component

--- src/components/DateInput.svelte
+++ src/components/DateInput.svelte
@@ -1,9 +1,17 @@
 <script>
     export let title;
     export let placeholder;
+    export let onChange;
+    let inputValue = "";
 </script>
 
 <div>
     <h4 style="color : #8B8B8B" class="font-bold text-xs mb-2">{title}</h4>
-    <input {placeholder} class="text-3xl w-36 h-16 rounded-md px-4 font-semibold outline-none" style="border: 1px solid #8B8B8B" />
+    <input
+        {placeholder}
+        class="text-3xl w-36 h-16 rounded-md px-4 font-semibold outline-none"
+        style="border: 1px solid #8B8B8B"
+        bind:value={inputValue}
+        on:input={() => onChange(inputValue, title)}
+    />
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,12 +3,32 @@
     import DateInput from "../components/DateInput.svelte";
     import Arrow from "../assets/icons/arrow.svg";
     import AgeResult from "../components/AgeResult.svelte";
+    import { useState } from "../hooks/hooks";
 
     const dateInputs = [
         { title: "DAY", placeholder: "DD" },
         { title: "MONTH", placeholder: "MM" },
         { title: "YEAR", placeholder: "YYYY" },
     ];
+
+    const [birthDay, setBirthDay] = useState(0);
+    const [birthMonth, setBirthMonth] = useState(0);
+    const [birthYear, setBirthYear] = useState(0);
+
+    const onChangeClick = (value, type) => {
+        const numericValue = Number(value);
+        if (type === "DAY") {
+            setBirthDay(numericValue);
+        } else if (type === "MONTH") {
+            setBirthMonth(numericValue);
+        } else {
+            setBirthYear(numericValue);
+        }
+    };
+
+    const onClick = () => {
+        console.log($birthDay, $birthMonth, $birthYear);
+    };
 </script>
 
 <div
@@ -17,20 +37,35 @@
 >
     <div class="dates flex gap-8 m-7">
         {#each dateInputs as items, index}
-            <DateInput title={items.title} placeholder={items.placeholder} />
+            <DateInput
+                title={items.title}
+                placeholder={items.placeholder}
+                onChange={onChangeClick}
+            />
         {/each}
     </div>
 
     <div class="flex flex-row items-center mr-6 ml-7">
         <hr class="w-full" style="background-color: #e7e7;" />
-        <div style="background-color: #864CFF;" class="w-fit rounded-full p-5">
+        <div
+            style="background-color: #864CFF;"
+            class="w-fit rounded-full p-5"
+            role="button"
+            on:click={() => onClick()}
+            on:keydown={(event) => {
+                if (event.key === "Enter") {
+                    onClick();
+                }
+            }}
+            tabindex="0"
+        >
             <img src={Arrow} alt="arrow" />
         </div>
     </div>
 
     <div class="ageResult ml-7 mb-7">
-        <AgeResult time="years"/>
-        <AgeResult time="months"/>
-        <AgeResult time="days"/>
+        <AgeResult time="years" />
+        <AgeResult time="months" />
+        <AgeResult time="days" />
     </div>
 </div>
